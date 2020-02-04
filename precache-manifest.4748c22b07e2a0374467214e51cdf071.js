2c5c384f573e4e032aa287ca67551c46d669b8f4
Amritha Marimuthu <amritha@Amrithas-MacBook-Air.local>
2020-02-04 18:20:48 -0500

    Updates

---
+++ precache-manifest.4748c22b07e2a0374467214e51cdf071.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f932ae58e1314f2569f6cf759ed3867b",
+    "url": "/first-react-app/index.html"
+  },
+  {
+    "revision": "c8852b44793fb0f49b88",
+    "url": "/first-react-app/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "88c57701d6a1d906654e",
+    "url": "/first-react-app/static/css/main.ce289add.chunk.css"
+  },
+  {
+    "revision": "c8852b44793fb0f49b88",
+    "url": "/first-react-app/static/js/2.8c911d9c.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/first-react-app/static/js/2.8c911d9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88c57701d6a1d906654e",
+    "url": "/first-react-app/static/js/main.f02d0fb6.chunk.js"
+  },
+  {
+    "revision": "8f8421dc9130ba9a9ff5",
+    "url": "/first-react-app/static/js/runtime-main.cb0de62a.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/first-react-app/static/media/SEGA.12a3db8b.woff"
+  }
+]);
